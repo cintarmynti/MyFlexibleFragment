6380249472ab205207aec8f651ead94d197a5467
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2024-10-06 01:00:03 +0700

    Flexible Fragment

--- app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
@@ -22,7 +22,16 @@ class HomeFragment : Fragment(), View.OnClickListener {
         val btnCategory:Button = view.findViewById(R.id.btn_category)
         btnCategory.setOnClickListener(this)
     }
-    override fun onClick(v: View) {
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
 
+        }
     }
 }
